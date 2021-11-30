33a7cc4b833a1eb8d0657b1789c704b3bf2b061c
rinera182@gmail.com <rinera182@gmail.com>
2021-12-01 07:40:56 +0900

    プロジェクト ファイルを追加します。

---
+++ Ex27_12_Honda/ConstructorSampe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ConsoleUtility;
+
+namespace ConstructorSampe
+{
+    class ConstructorSampeMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("三角形の３辺の長さＡ,Ｂ,Ｃを入力してください。");
+            Triangle triangle = new Triangle(
+                InteractiveInput.InputFloat("辺Ａの長さ："),
+                InteractiveInput.InputFloat("辺Ｂの長さ："),
+                InteractiveInput.InputFloat("辺Ｃの長さ："));
+            Console.WriteLine($"面積は{triangle.GetSurface()}");
+        }
+    }
+}
---
+++ Ex27_12_Honda/Triangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorSampe
+{
+    class Triangle
+    {
+        private float bottom;
+        private float height;
+        private float[] sideLength = new float[3];
+        private float surface;
+        /// <summary>
+        /// 三角形
+        /// </summary>
+        /// <param name="b">底辺</param>
+        /// <param name="h">高さ</param>
+        public Triangle(float b, float h)
+        {
+            this.bottom =b;
+            this.height = h;
+            surface = bottom * height / 2;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="side1">辺の長さＡ</param>
+        /// <param name="side2">辺の長さＢ</param>
+        /// <param name="side3">辺の長さＣ</param>
+        public Triangle(float side1, float side2, float side3)
+        {
+            this.sideLength[0] = side1;
+            this.sideLength[1] = side2;
+            this.sideLength[2] = side3;
+            float s = (sideLength[0] + sideLength[1] + sideLength[2]) / 2;
+            surface = MathF.Sqrt(s * (s - sideLength[0]) * (s - sideLength[1]) * (s - sideLength[2]));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>面積</returns>
+        public float GetSurface()
+        {
+            return surface;
+        }
+
+    }
+}
